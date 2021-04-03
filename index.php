f5088ae558288ef1d27da98f5da0bf0a25a4e0a1
jsbsjsjbs <81616741+jsbsjsjbs@users.noreply.github.com>
2021-04-03 16:45:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,137 +1,113 @@
 <!DOCTYPE html>
 <html>
-<!-- wega idroot--> 
-<h50>103.227.254.107</h50>
-<!-- wega do --> 
-<h50>104.248.147.84</h50>
-<!-- wega aws triall --> 
-<h50>13.228.70.212</h50>
-<!-- wega aws oprek --> 
-<h50>13.229.203.207</h50>
-<!-- kang malaysia --> 
-<h50>167.99.79.153</h50>
-<!-- kang malaysia2 --> 
-<h50>165.22.100.44</h50>
-<!-- killerssh --> 
-<h50>103.227.253.194</h50>
-<!-- killerssh2--> 
-<h50>45.84.58.151</h50>
-<!-- sshsedang --> 
-<h50>43.229.254.97</h50>
-<!-- adip 2--> 
-<h50>113.20.31.16</h50>
-<!-- lingssh 0--> 
-<h50>208.87.132.111</h50>
-<!-- malay santox --> 
-<h50>124.217.251.184</h50>
-<!-- haidityara 2--> 
-<h50>167.99.76.17</h50>
-<!-- andra as 2--> 
-<h50>103.152.118.248</h50>
-<!-- Ari Prabowo 0--> 
-<h50>43.229.254.119</h50>
-<!-- Ari Prabowo2 1--> 
-<h50>103.41.207.226</h50>
-<!-- izzam 2--> 
-<h50>103.41.207.226</h50>
-<!-- izzam 3--> 
-<h50>35.240.231.246</h50>
-<!-- ahmad jhe 1--> 
-<h50>103.65.237.112</h50>
-<!-- @AxxxR57 0--> 
-<h50>124.217.247.44</h50>
-<!-- @AxxxR572 0--> 
-<h50>165.232.172.11</h50>
-<!-- @AxxxR573 0--> 
-<h50>124.217.251.176</h50>
-<!-- @ifscexplicit 0--> 
-<h50>13.250.42.57</h50>
-<!-- inex 69 1--> 
-<h50>103.153.2.131</h50>
-<!-- enda subarki 2--> 
-<h50>13.229.211.125</h50>
-<!-- dicky setiawan buy vps--> 
-<h50>165.232.175.74</h50>
-<!-- dori 1--> 
-<h50>148.72.210.161</h50>
-<!-- dork buy vps--> 
-<h50>143.198.194.67</h50>
-<!-- pindo alfin--> 
-<h50>103.77.107.209</h50>
-<!-- badboy--> 
-<h50>124.217.246.67</h50>
-<!-- badboy2--> 
-<h50>143.198.220.20</h50>
-<!-- badboy3--> 
-<h50>159.65.11.196</h50>
-<!-- badboy4--> 
-<h50>165.22.252.89</h50>
-<!-- badboy5--> 
-<h50>139.99.90.178</h50>
-<!-- badboy6--> 
-<h50>165.232.164.131</h50>
-<!-- badboy7--> 
-<h50>43.225.108.143</h50>
-<!-- badboy8--> 
-<h50>159.65.10.195</h50>
-<!-- badboy9--> 
-<h50>51.79.159.107</h50>
-<!-- horas--> 
-<!--<h50>103.153.2.102</h50>-->
-<!-- rizal 0--> 
-<h50>116.251.216.254</h50>
-<!-- chieftoo buy vps--> 
-<h50>143.198.204.224</h50>
-<!-- nobita 0--> 
-<h50>51.79.204.243</h50>
-<!-- fm 0--> 
-<h50>43.229.254.157</h50>
-<!-- rague 0--> 
-<h50>52.148.86.64</h50>
-<!-- andra as buy vps--> 
-<h50>143.198.219.53</h50>
-<!-- aryazid holid buy vps--> 
-<h50>143.198.213.83</h50>
-<!-- ezue zahar--> 
-<h50>165.232.175.172</h50>
-<!-- riefid buy vps--> 
-<h50>167.99.65.223</h50>
-<!-- al khabib 1x--> 
-<h50>143.198.200.88</h50>
-<!-- yazid--> 
-<h50>13.250.17.87</h50>
-<!-- yuyus ruswandi buy vps--> 
-<h50>178.128.20.80</h50>
-<!-- santoso buy vps--> 
-<h50>159.65.136.95</h50>
-<!-- joss buy vps--> 
-<h50>209.97.169.30</h50>
-<!-- belalang buy vps --> 
-<h50>206.189.156.184</h50>
-<!-- randa --> 
-<h50>103.101.3.73</h50>
-<!-- randa --> 
-<h50>103.101.3.94</h50>
-<!-- idipv6 --> 
-<h50>103.219.251.153</h50>
-<!-- aldy suryana rohman --> 
-<h50>13.212.176.147</h50>
-<!-- aldy suryana rohman2 --> 
-<h50>34.87.123.132</h50>
-<!-- bhuana --> 
-<h50>103.226.138.44</h50>
-<!-- bhuana2 --> 
-<h50>45.32.110.51</h50>
-<!-- dork buy vps --> 
-<h50>206.189.144.45</h50>
-<!-- gas 1jt --> 
-<h50>13.229.53.117</h50>
-<!-- izzam --> 
-<h50>139.177.185.33</h50>
-<!-- izzam2 --> 
-<h50>34.101.229.45</h50>
-<!-- latifan --> 
-<h50>45.84.58.166</h50>
-<!-- dolpin --> 
-<h50>51.79.242.153</h50>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>kituVPN</title>
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+  
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href='<?php include ('sett/toolbar.php'); echo "$index"; ?>'>kituVPN</a>
+  <a href='<?php include ('sett/toolbar.php'); echo "$grup"; ?>'>Grup</a>
+  <a href='<?php include ('sett/toolbar.php'); echo "$admin"; ?>'>Admin</a>
+  <a href='<?php include ('sett/toolbar.php'); echo "$member"; ?>'>Member</a>
+  <a href='<?php include ('sett/toolbar.php'); echo "$triall"; ?>'>Triall</a>
+</div>
+<center><script data-cfasync='false' type='text/javascript' src='//p427884.clksite.com/adServe/banners?tid=427884_838753_0'></script></center>
+<div style="padding-left:16px">
+	<center>
+<h1>Layanan kituVPN</h1>
+    </center>
+<ul>
+        <li>SALDO PAYPAL
+            <ul>
+                <li>Rate 14k = 1$</li>
+            </ul>
+        </li>
+        <li>VPS DIGITALOCEAN
+            <ul>
+                <li>spek vps:</li>
+                <li>server singapore</li>
+                <li>1 GB / 1 CPU</li>
+                <li>25 GB SSD Disk</li>
+                <li>1000 GB transfer</li>
+                <li>OS: debian10, ubuntu18-20, centos7</li>
+                <li>sudah terinstall script premium</li>
+            </ul>
+        </li>
+        <li>VPS LINODE
+            <ul>
+                <li>spek vps:</li>
+                <li>server singapore</li>
+                <li>2GB/1CPU | 4GB/2CPU</li>
+                <li>25GB | 80GB (SSD Disk)</li>
+                <li>1000GB | 2000GB (transfer)</li>
+                <li>OS: debian10, ubuntu18-20, centos7</li>
+                <li>sudah terinstall script premium</li>
+            </ul>
+        </li>
+        <li>VPS GOOGLE CLOUDE
+            <ul>
+                <li>spek vps:</li>
+                <li>server singapore</li>
+                <li>1GB/2CPU | 2GB/2CPU</li>
+                <li>10 GB SSD Disk (bisa ditambahkan)</li>
+                <li>unlimited bandwidth</li>
+                <li>OS: debian10, ubuntu18-20, centos7</li>
+                <li>sudah terinstall script premium</li>
+            </ul>
+        </li>
+        <li>Script auto installer VPS premium
+            <ul>
+                <li>Fitur script premium bisa dilihat di link dibawah ini:</li>
+                <li>debian10 <a href="https://github.com/wegare123/deb10">klik disini</a></li>
+                <li>ubuntu18-20 <a href="https://github.com/wegare123/ubuntu18-20">klik disini</a></li>
+                <li>centos7 <a href="https://github.com/wegare123/centos7">klik disini</a></li>
+            </ul>
+        </li>
+        <li>Panel reseller (PROMO)
+            <ul>
+                <li>panel admin (mengelola server, harga/service, saldo/member, add/del member, list member, dan keterangan saldo member)</li>
+                <li>panel member (keterangan saldo dan buy service yang disediakan admin)</li>
+                <li>gratis panel reseller</li>
+            </ul>
+        </li>
+        <li>Jika berminat hubungi kontak dibawah:
+            <ul>
+                <li>Telegram +6282318259593</li>
+            </ul>
+        </li>
+    </ul>
+</div>
+<script data-cfasync='false' type='text/javascript' src='//p427880.clksite.com/adServe/banners?tid=427880_838746_3'></script>
+</body>
 </html>
